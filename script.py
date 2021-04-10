9152ad84c1c62a8ef97ab9981592ebe459acc57d
Brian Mann <mannb986@gmail.com>
2021-04-10 08:55:03 +0200

    code completed and tested successfully

--- script.py
+++ script.py
@@ -1,19 +1,50 @@
 def sparse_search(data, search_val):
-  print("Data: " + str(data))
-  print("Search Value: " + str(search_val))
+    print("Data: " + str(data))
+    print("Search Value: " + str(search_val))
 
-  first, last = 0, len(data)-1
+    first = 0 
+    last = len(data) - 1
 
-  while first <= last:
-      mid = (first + last) // 2
-      if not data[mid]:
-          left = mid -1
-          right = mid + 1
+    while first <= last:
+    
+        mid = (first + last) // 2
 
-          while(True):
-              if left < first and right > last:
-                  print("{0} is not in the dataset".format(search_val))
-                  return
+# Checking if the middle has empty data
+        if not data[mid]: 
+            left = mid -1
+            right = mid + 1
+
+            while(True):
+                if left < first and right > last:
+                    print("{0} is not in the dataset".format(search_val))
+                    return
+
+                elif right <= last and data[right]:
+                    mid = right
+                    break
+
+                elif left >= first and data[left]:
+                    mid = left
+                    break
+
+                right += 1
+                left -= 1
+
+# Checking if the search value is equal to the middle
+        if data[mid] == search_val:
+                print("{0} found at position {1}".format(search_val, mid))
+                return
+
+# Checking if the search value is less than the middle
+        elif search_val < data[mid]:
+            last = mid - 1
+
+# Checking if the search value is greater than the middle 
+        elif search_val > data[mid]:
+            first = mid + 1
+
+    print("{0} is not in the dataset".format(search_val))
+            
 
 
 
